c1f7317ea6584d7a8c27dca5b6c2297f72cd4d22
DamiSeggi <damiansegginger@gmail.com>
2026-03-11 08:54:05 +0100

    Model und DTO

---
+++ src/main/java/simpleNote/Model/Note.java
@@ -0,0 +1,33 @@
+package simpleNote.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "notes")
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String content;
+
+    public Note() {}
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+
+    public String getContent() { return content; }
+    public void setContent(String content) { this.content = content; }
+}
